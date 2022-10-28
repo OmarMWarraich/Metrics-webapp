75a1eb59fc4eb8f0cdc2b68a0c0964adcc728cb7
OmarMWarraich <earthling986@gmail.com>
2022-10-29 03:05:45 +0500

    style: apply design

--- src/features/details/Details.js
+++ src/features/details/Details.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Card } from 'react-bootstrap';
+import styles from './Details.module.css';
+import PieToBarChart from '../home/Pie2BarChart';
 
 const Details = () => {
   const Id = localStorage.getItem('id');
@@ -9,221 +11,271 @@ const Details = () => {
 
   return (
     <>
-      <div className="container">
+      <div className={styles.container}>
         {details.map((item) => (
-          <Card key={item.CoinInfo.Id} id={item.CoinInfo.Id} className="card">
-            <Card.Body className="cardBody">
-              <div className="img_container">
-                <Card.Img className="image" src={`https://www.cryptocompare.com${item.CoinInfo.ImageUrl}`} />
-              </div>
-              <div className="cardDetails">
-                <div>
-                  <Card.Title className="title">{item.CoinInfo.FullName}</Card.Title>
+          <div key={item.CoinInfo.Id}>
+            <Card
+              className={styles.card}
+            >
+              <Card.Body
+                className={styles.cardBody}
+                style={{ border: '2px solid #002147' }}
+              >
+                <div className={styles.img_container}>
+                  <PieToBarChart />
                 </div>
-                <div>
+                <div
+                  className={styles.cardDetails}
+                >
                   <div>
-                    <Card.Text className="text">
-                      <strong>Rank</strong>
-                      {item.CoinInfo.FullName}
-                    </Card.Text>
+                    <Card.Title className={styles.title}>{item.CoinInfo.Name}</Card.Title>
                   </div>
                   <div>
-                    <Card.Text className="text">
-                      <strong>FullName</strong>
-                      {item.CoinInfo.FullName}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Price</strong>
-                      {item.RAW.USD.PRICE}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Market Cap</strong>
-                      {item.RAW.USD.MKTCAP}
-                    </Card.Text>
-                  </div>
-
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Algorithm:</strong>
-                      {item.CoinInfo.Algorithm}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>ProofType:</strong>
-                      {item.CoinInfo.ProofType}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Rating:</strong>
-                      {item.CoinInfo.Rating.Weiss.Rating}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>TAR:</strong>
-                      {item.CoinInfo.Rating.Weiss.TechnologyAdoptionRating}
-                    </Card.Text>
-                  </div>
-
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Block Time:</strong>
-                      {item.CoinInfo.BlockTime}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Block Reward:</strong>
-                      {item.CoinInfo.BlockReward}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Asset Launch Date:</strong>
-                      {item.CoinInfo.AssetLaunchDate}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Max Supply:</strong>
-                      {item.CoinInfo.MaxSupply}
-                    </Card.Text>
-                  </div>
-
-                  <div>
-                    <Card.Text className="text">
-                      <strong>From Symbol:</strong>
-                      {item.DISPLAY.USD.FROMSYMBOL}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>To Symbol:</strong>
-                      {item.DISPLAY.USD.TOSYMBOL}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Market:</strong>
-                      {item.DISPLAY.USD.MARKET}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Price:</strong>
-                      {item.DISPLAY.USD.PRICE}
-                    </Card.Text>
-                  </div>
-
-                  <div>
-                    <Card.Text className="text">
-                      <strong>LAST Volume:</strong>
-                      {item.DISPLAY.USD.LASTVOLUME}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Volume 24 hour</strong>
-                      {item.DISPLAY.USD.VOLUME24HOUR}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Volume 24 hour to:</strong>
-                      {item.DISPLAY.USD.VOLUME24HOURTO}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Volume 24 hour from:</strong>
-                      {item.DISPLAY.USD.OPEN24HOUR}
-                    </Card.Text>
-                  </div>
-
-                  <div>
-                    <Card.Text className="text">
-                      <strong>High 24 hour:</strong>
-                      {item.DISPLAY.USD.HIGH24HOUR}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Low 24 hour:</strong>
-                      {item.DISPLAY.USD.LOW24HOUR}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Last Market:</strong>
-                      {item.DISPLAY.USD.LASTMARKET}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Change 24 hour:</strong>
-                      {item.DISPLAY.USD.CHANGE24HOUR}
-                    </Card.Text>
-                  </div>
-
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Change Percent 24 hour:</strong>
-                      {item.DISPLAY.USD.CHANGEPCT24HOUR}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Change 24 hour:</strong>
-                      {item.DISPLAY.USD.CHANGE24HOUR}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Change hour %:</strong>
-                      {item.DISPLAY.USD.CHANGEPCTHOUR}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Change hour:</strong>
-                      {item.DISPLAY.USD.CHANGEHOUR}
-                    </Card.Text>
-                  </div>
-
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Supply:</strong>
-                      {item.DISPLAY.USD.SUPPLY}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Mkt Cap:</strong>
-                      {item.DISPLAY.USD.MKTCAP}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Total Volume 24 hour:</strong>
-                      {item.DISPLAY.USD.TOTALVOLUME24H}
-                    </Card.Text>
-                  </div>
-                  <div>
-                    <Card.Text className="text">
-                      <strong>Total Volume 24 hour to:</strong>
-                      {item.DISPLAY.USD.TOTALVOLUME24HTO}
-                    </Card.Text>
+                    <div>
+                      <Card.Text className={styles.text}>
+                        {item.CoinInfo.Id}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.text}>
+                        {item.CoinInfo.FullName}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.text}>
+                        {item.CoinInfo.Price}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.text}>
+                        $(B)
+                        {' '}
+                        {(item.RAW.USD.MKTCAP / 1000000000).toFixed(2)}
+                      </Card.Text>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Card.Body>
-          </Card>
+              </Card.Body>
+            </Card>
+            <Card key={item.CoinInfo.Id} id={item.CoinInfo.Id} className={styles.card2}>
+              <Card.Body className={styles.cardLBody}>
+                <div className={styles.cardLDetails}>
+                  <div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Algorithm:</strong>
+                        {item.CoinInfo.Algorithm}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>ProofType:</strong>
+                        {item.CoinInfo.ProofType}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Rating:</strong>
+                        {item.CoinInfo.Rating.Weiss.Rating}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>TAR:</strong>
+                        {item.CoinInfo.Rating.Weiss.TechnologyAdoptionRating}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+
+              <Card.Body className={styles.cardLBody}>
+                <div className={styles.cardLDetails}>
+                  <div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Block Time:</strong>
+                        {item.CoinInfo.BlockTime}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Block Reward:</strong>
+                        {item.CoinInfo.BlockReward}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Asset Launch Date:</strong>
+                        {item.CoinInfo.AssetLaunchDate}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Max Supply:</strong>
+                        {item.CoinInfo.MaxSupply}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+
+              <Card.Body className={styles.cardLBody}>
+                <div className={styles.cardLDetails}>
+                  <div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>From Symbol:</strong>
+                        {item.DISPLAY.USD.FROMSYMBOL}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>To Symbol:</strong>
+                        {item.DISPLAY.USD.TOSYMBOL}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Market:</strong>
+                        {item.DISPLAY.USD.MARKET}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Price:</strong>
+                        {item.DISPLAY.USD.PRICE}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+
+              <Card.Body className={styles.cardLBody}>
+                <div className={styles.cardLDetails}>
+                  <div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>LAST Volume:</strong>
+                        {item.DISPLAY.USD.LASTVOLUME}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Volume 24 hour</strong>
+                        {item.DISPLAY.USD.VOLUME24HOUR}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Volume 24 hour to:</strong>
+                        {item.DISPLAY.USD.VOLUME24HOURTO}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Volume 24 hour from:</strong>
+                        {item.DISPLAY.USD.OPEN24HOUR}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+
+              <Card.Body className={styles.cardLBody}>
+                <div className={styles.cardLDetails}>
+                  <div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>High 24 hour:</strong>
+                        {item.DISPLAY.USD.HIGH24HOUR}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Low 24 hour:</strong>
+                        {item.DISPLAY.USD.LOW24HOUR}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Last Market:</strong>
+                        {item.DISPLAY.USD.LASTMARKET}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Change 24 hour:</strong>
+                        {item.DISPLAY.USD.CHANGE24HOUR}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+
+              <Card.Body className={styles.cardLBody}>
+                <div className={styles.cardLDetails}>
+                  <div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Change Percent 24 hour:</strong>
+                        {item.DISPLAY.USD.CHANGEPCT24HOUR}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Change 24 hour:</strong>
+                        {item.DISPLAY.USD.CHANGE24HOUR}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Change hour %:</strong>
+                        {item.DISPLAY.USD.CHANGEPCTHOUR}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Change hour:</strong>
+                        {item.DISPLAY.USD.CHANGEHOUR}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+
+              <Card.Body className={styles.cardLBody}>
+                <div className={styles.cardLDetails}>
+                  <div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Supply:</strong>
+                        {item.DISPLAY.USD.SUPPLY}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Mkt Cap:</strong>
+                        {item.DISPLAY.USD.MKTCAP}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Total Volume 24 hour:</strong>
+                        {item.DISPLAY.USD.TOTALVOLUME24H}
+                      </Card.Text>
+                    </div>
+                    <div>
+                      <Card.Text className={styles.LText}>
+                        <strong>Total Volume 24 hour to:</strong>
+                        {item.DISPLAY.USD.TOTALVOLUME24HTO}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
         ))}
       </div>
     </>
--- src/features/navBar/Navbar.js
+++ src/features/navBar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { faGear, faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -10,15 +12,24 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ backgroundColor: 'darkBlue' }}>
       <div>
-        <button type="button" onClick={toHome}>🔚</button>
+        <button type="button" onClick={toHome} style={{ backgroundColor: 'darkblue', border: 'none', color: '#fff' }}>
+          {'<'}
+          {' '}
+          2022
+        </button>
       </div>
       <div>
         {localStorage.getItem('id') === null ? 'overall statistics' : 'coin stats'}
       </div>
-      <div>
-        🔥
+      <div style={{
+        display: 'flex', justifyContent: 'space-evenly', width: '20%',
+      }}
+      >
+        <FontAwesomeIcon icon={faMicrophone} />
+        {' '}
+        <FontAwesomeIcon icon={faGear} />
       </div>
     </div>
   );
